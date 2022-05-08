e6b818b9c18821b37e50b10e3f0d291ee44421ee
Your Name <ahmetyildirir1@gmail.com>
2022-05-08 23:01:26 +0300

    gg
    gg

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,12 @@
 from tempfile import template
 from django.urls import path
-from .views import homepage
+from .views import homepage,newpage
 
 
 
 urlpatterns = [
    
-    path('',homepage,name='homepage'),
-   
-      
+    path('home',homepage,name='homepage'),
+    path('new',newpage,name='newpage')
+    
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def homepage(request):
     return render(request,'home.html')
 
+
+def newpage(request):
+    return render(request,'newpage.html')
+
