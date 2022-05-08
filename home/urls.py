f3b7942933cd81e2c88ae50c901e3b8c0cac8268
Your Name <ahmetyildirir1@gmail.com>
2022-05-08 15:46:43 +0300

    gonder

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
+from tempfile import template
 from django.urls import path
-from .views import homepage
+from .views import homepage,team
 
 
 
 urlpatterns = [
    
-    path('',homepage,name='homepage')
+    path('',homepage,name='homepage'),
+    path('team',team,name='team')
       
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request,'about.html')
+
+def team(request):
+    return render(request,'team.html')
